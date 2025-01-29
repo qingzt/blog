38d25e2d3d3a201e4ce16f67615d62b4b8a0e6ea
qingzt <754307254@qq.com>
2025-01-29 23:50:36 +0800

    update

--- update_discussion.py
+++ update_discussion.py
@@ -1,15 +1,23 @@
 from models.discussion import Discussion, DiscussionModel
 from models.discussion_label import Discussion_Label, Discussion_LableModel
 from exts import db
+import os
 
 if __name__ == "__main__":
     db.create_tables([DiscussionModel, Discussion_LableModel])
     discussion = Discussion.fromEnv()
     discussion_labels = Discussion_Label.fromEnv()
     print(f"discussion: {discussion}\n discussion_labels: {discussion_labels}")
-    discussion_model = DiscussionModel.fromDiscussion(discussion)
-    num_rows = discussion_model.save(force_insert=True)
-    for discussion_label in discussion_labels:
-        discussion_label_model = Discussion_LableModel.fromDiscussion_Label(discussion_label)
-        num_rows += discussion_label_model.save(force_insert=True)
-    print(f"Update {num_rows} rows")
+    action = os.environ.get("ACTION")
+    if action == "deleted":
+        num_rows = DiscussionModel.delete().where(DiscussionModel.id == discussion.id).execute()
+        num_rows += Discussion_LableModel.delete().where(Discussion_LableModel.discussion_id == discussion.id).execute()
+        print(f"Delete {num_rows} rows")
+    else:
+        discussion_model = DiscussionModel.fromDiscussion(discussion)
+        num_rows = discussion_model.save(force_insert=True)
+        for discussion_label in discussion_labels:
+            discussion_label_model = Discussion_LableModel.fromDiscussion_Label(discussion_label)
+            num_rows += discussion_label_model.save(force_insert=True)
+        print(f"Update {num_rows} rows")
+    db.close()
--- update_label.py
+++ update_label.py
@@ -1,10 +1,17 @@
 from models.lable import Label, LabelModel
 from exts import db
+import os
 
 if __name__ == "__main__":
     db.create_tables([LabelModel])
     label = Label.fromEnv()
     print(label)
-    label_model = LabelModel.fromLabel(label)
-    num_rows = label_model.save(force_insert=True)
-    print(f"Update {num_rows} rows")
+    action = os.environ.get("ACTION")
+    if action == "deleted":
+        num_rows = LabelModel.delete().where(LabelModel.id == label.id).execute()
+        print(f"Delete {num_rows} rows")
+    else:
+        label_model = LabelModel.fromLabel(label)
+        num_rows = label_model.save(force_insert=True)
+        print(f"Update {num_rows} rows")
+    
